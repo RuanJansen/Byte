74844b55981009bb59a36196686770c763931c24
RuanJanen <69080617+RuanJanen@users.noreply.github.com>
2020-08-07 12:03:51 +0200

    Add files via upload

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,30 +1,51 @@
 package com.example.abyte;
-
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity  {
     Button btnNext;
+    TextView textviewRegister;
+    EditText email, password;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.byte_login);
-
+        getLogin();
+        chechLogin();
 
         btnNext = (Button)findViewById(R.id.btnNext);
-
-
+        textviewRegister = (TextView)findViewById(R.id.textviewRegester);
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("click");
+                //System.out.println("click");
             }
         });
-        }
+        textviewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //System.out.println("click");
+            }
+        });
+    }
+
+    public void getLogin(){
+        email = (EditText)findViewById(R.id.edtEmail);
+        password = (EditText)findViewById(R.id.edtPassword);
+     }
+
+     public void chechLogin(){
+         //System.out.println(email+" "+password);
+     }
+
+
+
+
+
 
 
 }
