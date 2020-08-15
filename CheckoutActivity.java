560906a701f46467ad66f0c2e8ad378ecaa20732
RuanJanen <69080617+RuanJanen@users.noreply.github.com>
2020-08-16 00:19:52 +0200

    Add files via upload

--- CheckoutActivity.java
+++ CheckoutActivity.java
@@ -1,32 +1,110 @@
 package com.example.bytev2;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 public class CheckoutActivity extends AppCompatActivity {
+    public static String globalHour = null;
+    public static String globalMinute = null;
     Button btnPay;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.byte_checkout);
+        btnPay = (Button) findViewById(R.id.btnPay);
+        EditText edtHour;
+        edtHour = (EditText) findViewById(R.id.edtHour);
+        EditText edtMinute;
+        edtMinute = (EditText) findViewById(R.id.edtMinute);
+
+        System.out.println(globalHour + ":" + globalMinute);
         init();
-        btnPay = (Button)findViewById(R.id.btnPay);
+
         btnPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), PaymentActivity.class);
-                startActivityForResult(myIntent, 0);
+                if(CheckInputBlank()){
+                    globalMinute = String.valueOf(edtMinute);
+                    if (CheckTimeInput()) {
+                        globalHour = String.valueOf(edtHour);
+                        Intent myIntent = new Intent(view.getContext(), PaymentActivity.class);
+                        startActivityForResult(myIntent, 0);
+                    }
+                }else{
+                    AlertDialog.Builder builderDialog = new AlertDialog.Builder(CheckoutActivity.this);
+                    builderDialog.setMessage("Enter a collection time!").setCancelable(false).setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            System.out.println("AlertDialog message close");
+                        }
+                    });
+                    AlertDialog alertDialog = builderDialog.create();
+                    alertDialog.show();
+                }
+
             }
         });
-
     }
+    private boolean CheckInputBlank(){
+        String blank = "";
+        boolean check1 = false, check2 = false;
+
+        EditText edtHour;
+        edtHour = (EditText) findViewById(R.id.edtHour);
+        EditText edtMinute;
+        edtMinute = (EditText) findViewById(R.id.edtMinute);
+
+        String hour= String.valueOf(edtHour.getText());
+        String minute = String.valueOf(edtMinute.getText());
+
+        if(hour.equals(blank)){
+            //check1 = true;
+            System.out.println("blank1");
+            return false;
+        }
+        if(minute.equals(blank)){
+            //check2 = true;
+            System.out.println("blank2");
+            return false;
+        }
+//        if(check1 || check2){
+//            return false;
+//        }
+        System.out.println("not blank");
+        return true;
+    }
+
+    private boolean CheckTimeInput() {
+        EditText edtHour;
+        edtHour = (EditText) findViewById(R.id.edtHour);
+        EditText edtMinute;
+        edtMinute = (EditText) findViewById(R.id.edtMinute);
+        int ihour, iminute;
+        ihour = Integer.parseInt(String.valueOf(edtHour.getText()));
+        iminute = Integer.parseInt(String.valueOf(edtMinute.getText()));
+        boolean check1 = false, check2 = false;
+        if(ihour<=23){
+            check1 = true;
+        }
+        if(iminute<=59){
+            check2 = true;
+        }
+        if((check1) && (check2)){
+            return true;
+        }
+        return false;
+    }
+
     public void init() {
         TableLayout tablelayoutOrder = (TableLayout) findViewById(R.id.tablelayoutOrder);
         TextView textviewOrderNum = new TextView(this);
